4998d474cae3ac3f108f150e989986d55096ca42
David MacMahon <davidm@astro.berkeley.edu>
2022-07-24 16:36:00 +0000

    Add create_fdr(spectrogram, Nr::Integer) method

--- src/DopplerDriftSearch.jl
+++ src/DopplerDriftSearch.jl
@@ -14,14 +14,23 @@ export fftfdr, fftfdr!
 
 """
 Create an uninitialized `Matrix` suitable for use with `intfdr!` or `fftfdr!`
+and the given `spectrogram` and `Nr` (number of rates).  The returned `Matrix`
+will be similar to `spectrogram` in type and size of first dimension, but its
+second dimension will be `Nr`.
+"""
+function create_fdr(spectrogram, Nr::Integer)
+    Nf = size(spectrogram, 1)
+    similar(spectrogram, Nf, Nr)
+end
+
+"""
+Create an uninitialized `Matrix` suitable for use with `intfdr!` or `fftfdr!`
 and the given `spectrogram` and `rates`.  The returned `Matrix` will be similar
 to `spectrogram` in type and size of first dimension, but its second dimension
 will be sized by the length of `rates`.
 """
 function create_fdr(spectrogram, rates)
-    Nf = size(spectrogram, 1)
-    Nr = length(rates)
-    similar(spectrogram, Nf, Nr)
+    create_fdr(spectrogram, length(rates))
 end
 
 include("intfdr.jl")
